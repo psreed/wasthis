62c5a5cb2bbcec13498c6f5cc74ecc30134c49be
Paul Reed <paul.reed@puppet.com>
2020-01-10 15:05:40 -0500

    update tasks/plans for better descriptinve naming

---
+++ plans/install_ibm_package_utility.pp
@@ -0,0 +1,9 @@
+#Plan to install IBM Package Utility
+plan wasthis::install_ibm_package_utility (
+  TargetSpec $targets,
+  String     $package_utility_zipfile
+) {
+  run_command('mkdir -p /opt/was_installers',$targets, '_run_as' => 'root')
+  upload_file($package_utility_zipfile,'/opt/was_installers',$targets, '_run_as' => 'root')
+  run_task('install_ibm_package_utility',$targets, '_run_as' => 'root')
+}

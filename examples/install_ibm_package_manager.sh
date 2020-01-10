1ab5a99b37fc98a0a4858400dccefec11f47e6b3
Paul Reed <paul.reed@puppet.com>
2020-01-10 15:25:52 -0500

    updates to plan

---
+++ examples/install_ibm_package_manager.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+bolt plan run wasthis::install_ibm_package_utility -t initechnix0.classroom.puppet.com --params @params.json
--- plans/install_ibm_package_utility.pp
+++ plans/install_ibm_package_utility.pp
@@ -4,6 +4,6 @@ plan wasthis::install_ibm_package_utility (
   String     $package_utility_zipfile
 ) {
   run_command('mkdir -p /opt/was_installers',$targets, '_run_as' => 'root')
-  upload_file($package_utility_zipfile,'/opt/was_installers',$targets, '_run_as' => 'root')
-  run_task('install_ibm_package_utility',$targets, '_run_as' => 'root')
+  upload_file($package_utility_zipfile,'/opt/was_installers/',$targets, '_run_as' => 'root')
+  run_task('wasthis::install_ibm_package_utility',$targets, '_run_as' => 'root')
 }
